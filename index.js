690511d35cb6bbed17b6f596b5867096ff399256
Yago Fogaça <99728003+YagoFogaca@users.noreply.github.com>
2022-07-02 14:02:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
 const prompt = require('prompt-sync')();
+console.clear();
 
 let sim = 0;
+
 let nao = 0;
+
 let respostas = [];
-let pergunta1 = prompt('Pergunta - 1 ').toLowerCase();
+
+let pergunta1 = prompt(
+    'Você estava com a vitima na noite do crime? ',
+).toLowerCase();
+
 while (pergunta1 != 'sim' && pergunta1 != 'nao') {
-    pergunta1 = prompt('Pergunta - 1 ').toLowerCase();
+    pergunta1 = prompt(
+        'Você estava com a vitima na noite do crime? ',
+    ).toLowerCase();
 }
-let pergunta2 = prompt('Pergunta - 2 ').toLowerCase();
+
+console.clear();
+
+let pergunta2 = prompt('Você conversou com ela nessa noite? ').toLowerCase();
+
 while (pergunta2 != 'sim' && pergunta2 != 'nao') {
-    pergunta2 = prompt('Pergunta - 2 ').toLowerCase();
+    pergunta2 = prompt('Você conversou com ela nessa noite? ').toLowerCase();
 }
-let pergunta3 = prompt('Pergunta - 3 ').toLowerCase();
+
+console.clear();
+
+let pergunta3 = prompt('Vocês brigaran? ').toLowerCase();
+
 while (pergunta3 != 'sim' && pergunta3 != 'nao') {
-    pergunta3 = prompt('Pergunta - 3 ').toLowerCase();
+    pergunta3 = prompt('Vocês brigaran? ').toLowerCase();
 }
-let pergunta4 = prompt('Pergunta - 4 ').toLowerCase();
+
+console.clear();
+
+let pergunta4 = prompt('Você gritou com ela? ').toLowerCase();
+
 while (pergunta4 != 'sim' && pergunta4 != 'nao') {
-    pergunta4 = prompt('Pergunta - 4 ').toLowerCase();
+    pergunta4 = prompt('Você gritou com ela? ').toLowerCase();
 }
-let pergunta5 = prompt('Pergunta - 5 ').toLowerCase();
+
+console.clear();
+
+let pergunta5 = prompt('Você tocou nela? ').toLowerCase();
+
 while (pergunta5 != 'sim' && pergunta5 != 'nao') {
-    pergunta5 = prompt('Pergunta - 5 ').toLowerCase();
+    pergunta5 = prompt('Você tocou nela? ').toLowerCase();
 }
+
+console.clear();
+
 respostas = [pergunta1, pergunta2, pergunta3, pergunta4, pergunta5];
+
 for (let i = 0; i < respostas.length; i++) {
     if (respostas[i] == 'sim') {
         sim++;
@@ -31,6 +60,7 @@ for (let i = 0; i < respostas.length; i++) {
         nao++;
     }
 }
+
 if (sim >= 4) {
     console.log('Vocé é CULPADO!');
 } else if (sim == 3) {
